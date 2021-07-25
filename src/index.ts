35587aa7fd95d16177821447e3f8644fbacfdbdd
initial commit <>
2021-07-25 19:53:38 +0900

    オプションパラメーターとデフォルトパラメーター

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,10 @@
 // unknownSample()
 
 // 04.関数の型定義
-import { logMessage, logMessage2, logMessage3, logMessage4, alwaysThrowError } from './function/basic'
+import { logMessage } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2 } from './function/parameters'
+
 logMessage('Hello TypeScript!')
-logMessage2('Hello TypeScript!')
-logMessage3('Hello TypeScript!')
-logMessage4('Hello TypeScript!')
-alwaysThrowError('Hello TypeScript!')
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
