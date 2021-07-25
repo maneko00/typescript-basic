0b52f8a7f2f74bcf2d125f2f3c6cec6cd61f986f
initial commit <>
2021-07-25 19:40:48 +0900

    関数の型定義

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,16 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { notExistSample, primitiveSample, anySample, unknownSample } from './basic'
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// import { notExistSample, primitiveSample, anySample, unknownSample } from './basic'
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
+
+// 04.関数の型定義
+import { logMessage, logMessage2, logMessage3, logMessage4, alwaysThrowError } from './function/basic'
+logMessage('Hello TypeScript!')
+logMessage2('Hello TypeScript!')
+logMessage3('Hello TypeScript!')
+logMessage4('Hello TypeScript!')
+alwaysThrowError('Hello TypeScript!')
