b82c513dba128ffc7652fdd12818beb793ce2eda
initial commit <>
2021-07-26 07:20:17 +0900

    合併型(union)と交差型(intersection)

--- src/object/alias.ts
+++ src/object/alias.ts
@@ -19,4 +19,54 @@ export default function typeAliasSample() {
     name: 'United States of America',
   }
   console.log('Object alias sample 2:', america)
+
+  // 合併型(union)と交差型(intersection)
+  type Knight = {
+    hp: number
+    sp: number
+    weapon: string
+    swordSkill: string
+  }
+
+  type Wizard = {
+    hp: number
+    mp: number
+    weapon: string
+    magicSkill: string
+  }
+
+  type Adventurer = Knight | Wizard // 合併型: KnightとWizardどちらかの型を持つ
+  type Paladin = Knight & Wizard // 交差型: KnightとWizardが持つ型を全て持っている
+
+  // Knightの型を持つadventurer2
+  const adventurer1: Adventurer = {
+    hp: 100,
+    sp: 30,
+    weapon: '木の剣',
+    swordSkill: '三連斬り',
+  }
+
+  console.log('Object alias sample 3:', adventurer1)
+
+  // Wizardの型を持つadventurer2
+  const adventurer2: Adventurer = {
+    hp: 100,
+    mp: 30,
+    weapon: '木の杖',
+    magicSkill: 'ファイヤボール',
+  }
+
+  console.log('Object alias sample 4:', adventurer2)
+
+  // KnightとWizard両方の型を持つpaladin
+  const paladin: Paladin = {
+    hp: 300,
+    sp: 100,
+    mp: 100,
+    weapon: '銀の剣',
+    swordSkill: '三連斬り',
+    magicSkill: 'ファイヤボール',
+  }
+
+  console.log('Object alias sample 5:', paladin)
 }
