587a340fd9da61ceb489eb2536d6f781d833ee0c
initial commit <>
2021-07-26 18:54:05 +0900

    reduce関数の型をジェネリックスで共通化

--- src/generics/basic.ts
+++ src/generics/basic.ts
@@ -19,4 +19,36 @@ export default function genericsSample() {
     return result
   }
   console.log('Generics basic sample 2:', numberReduce([100, 200, 300], 1000))
+
+  // 型エイリアスを作るとしたら...？
+  type Reduce = {
+    (array: string[], initialValue: string): string
+    (array: number[], initialValue: number): number
+  }
+
+  // ジェネリック型でまとめる
+  type GenericReduce<T> = {
+    (array: T[], initialValue: T): T
+  }
+
+  // ジェネリック型を使った呼び出し
+  const genericStringReduce: GenericReduce<string> = (array, initialValue) => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result += item
+    }
+    return result
+  }
+  console.log('Generics basic sample 3:', genericStringReduce(['MAKE ', 'TYPESCRIPT ', 'GREAT ', 'AGAIN'], ''))
+
+  const genericNumberReduce: GenericReduce<number> = (array, initialValue) => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result += item
+    }
+    return result
+  }
+  console.log('Generics basic sample 4:', numberReduce([-100, -200, -300], 1000))
 }
